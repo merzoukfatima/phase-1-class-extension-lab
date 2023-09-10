1da438b3955b5137639f514c7bdb28b13ea7c9da
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-09-10 20:15:47 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+class Polygon{
+    constructor(array) {
+        this.array=array
+    }
+    get countSides() {
+        return this.array.length
+    }
+    get perimeter() {
+        return this.array.reduce((total,item)=>(total+=item))
+    }
+}
+class Triangle extends Polygon{
+    get isValid() {
+        const [a,b,c]=this.array
+        return (a+b>c && a+c>b && b+c>a) ? true:false
+    }
+}
+
+class Square extends Polygon{
+    get isValid() {
+        const [a,b,c,d]=this.array
+        return (a===b && a===c && a===d) ? true:false
+    }
+    get area() {
+        return Math.pow(this.array[0],2)
+    }
+}
